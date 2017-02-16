341ba198d7451aa2551593956ead3fbcc3668ab5
Robert Deans <robdeans@gmail.com>
2017-02-15 21:03:12 -0500

    adds a bunch of files manually

---
+++ TrainNapper/GoogleMapsBridging.h
@@ -0,0 +1,19 @@
+//
+//  GoogleMapsBridging.h
+//  TrainNapper
+//
+//  Created by Robert Deans on 2/15/17.
+//  Copyright © 2017 Robert Deans. All rights reserved.
+//
+
+#ifndef GoogleMapsBridging_h
+#define GoogleMapsBridging_h
+
+#import "GMUMarkerClustering.h"
+
+//#import <Google-Maps-iOS-Utils/Clustering/GMUMarkerClustering.h>
+
+//#import "Google-Maps-iOS-Utils/GMUMarkerClustering.h"
+//#import <Goog le-Maps-iOS-Utils/GQTPointQuadTree.h>
+
+#endif /* GoogleMapsBridging_h */
